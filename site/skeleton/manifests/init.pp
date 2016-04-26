afd9e3e1873ce9cdac748a975a4ba771606280dd
Brandon C <bcomouche@costco.com>
2016-04-26 11:35:46 -0700

    Create init.pp
    Base file creation

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,18 @@
+class skeleton {
+
+  file { '/etc/skel':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode   => '755',
+  }
+
+  file { '/etc/skel/.bashrc':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode   => '755',
+    source  => "puppet:///modeules/skeleton/files/bashrc"
+  }
+
+}
